bb4e13fa83d91bf69cf89e111e552e160635e1a7
Ayodele David <dayodele89@outlook.com>
2022-05-19 21:00:42 +0100

    Added user creation tests

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
+User=get_user_model()
 # Create your tests here.
+class UserTest(TestCase):
+
+
+#Create base user test
+    def test_createuser(self):
+
+        user=User.objects.create_user(email='david@gmail.com',password='david123bcd',fullname="Ayodele David")
+
+        self.assertEqual(user.fullname,'Ayodele David')
+
+
+
+#Create staff user test
+    def test_create_staffuser(self):
+        user=User.objects.create_staffuser(email='david@gmail.com',password='david123bcd',fullname="Ayodele David")
+        self.assertEqual(user.fullname,'Ayodele David')
+        self.assertTrue(user.is_staff)
+
+#Create super user tests
+    def test_create_superuser(self):
+        user=User.objects.create_superuser(email='david@gmail.com',password='david123bcd',fullname="Ayodele David")
+        self.assertEqual(user.fullname,'Ayodele David')
+        self.assertTrue(user.is_superuser)
+    
